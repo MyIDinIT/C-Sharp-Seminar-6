b34efbef3aa492135581d4799ac4ee535fadbdc6
IvanZH <ivzh.nn@gmail.com>
2023-02-21 12:11:22 +0300

    task40 completed

---
+++ task40/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:   ");
+int A=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:   ");
+int B=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:   ");
+int C=Convert.ToInt32(Console.ReadLine());
+
+if (A+B<C || B+C<A || C+A<B)
+{
+    Console.WriteLine("Невозможный треугольник!");
+}
+
+else
+{
+    Console.WriteLine("Таким треугольник может быть :) ");
+}  
