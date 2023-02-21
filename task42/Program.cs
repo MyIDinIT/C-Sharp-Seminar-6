e8fbcb06fffcfe40152db7979cbacad191dea5d2
IvanZH <ivzh.nn@gmail.com>
2023-02-21 13:19:35 +0300

    task42 completed

---
+++ task42/Program.cs
@@ -0,0 +1,11 @@
+﻿void DevideByTwo(int x)
+{
+    if (x == 0) return;
+
+    DevideByTwo(x/2);
+    Console.Write(x%2);
+}
+Console.WriteLine("Введите  число:   ");
+int A=Convert.ToInt32(Console.ReadLine());
+DevideByTwo(A);
+
