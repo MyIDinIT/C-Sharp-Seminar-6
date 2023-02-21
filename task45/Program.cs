9560f878e3fcb0422798c60ed74d82d9613e7e96
IvanZH <ivzh.nn@gmail.com>
2023-02-21 14:14:47 +0300

    task45  completed

---
+++ task45/Program.cs
@@ -0,0 +1,19 @@
+﻿
+void FillArrayRandom(int[] X)
+{
+   for (int i = 0; i < X.Length; i++)
+   {
+    X[i]=new Random().Next(0,100);
+   } 
+}
+
+int[] array=new int[5];
+FillArrayRandom(array);
+Console.WriteLine(string.Join(",",array));
+
+int[] Newarray=new int[5];
+for (int i = 0; i < Newarray.Length; i++)
+{
+    Newarray[i]=array[i];
+}
+Console.WriteLine(string.Join(",",Newarray));
