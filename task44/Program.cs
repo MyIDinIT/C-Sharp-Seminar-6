244eda33d509eb42e1bc7250dd073f57cded8c2c
IvanZH <ivzh.nn@gmail.com>
2023-02-21 13:35:08 +0300

    task44 completed

---
+++ task44/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N:   ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] FibArray = new int[N];
+FibArray[0]=0;
+FibArray[1]=1;
+int count=2;
+while (count<N)
+{
+    FibArray[count]=FibArray[count-2]+FibArray[count-1];
+    count++;
+}
+Console.WriteLine(string.Join(",", FibArray));
